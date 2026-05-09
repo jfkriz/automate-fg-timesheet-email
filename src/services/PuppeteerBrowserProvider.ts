20bee54d49c4175c94a5fe6b801806821b81ae77
Jim Kriz <jim@kriz.net>
2026-05-08 20:22:29 -0400

    feat: support PUPPETEER_EXECUTABLE_PATH for Docker Chromium

--- src/services/PuppeteerBrowserProvider.ts
+++ src/services/PuppeteerBrowserProvider.ts
@@ -5,6 +5,7 @@ import type { IBrowserProvider } from '../interfaces/IBrowserProvider';
 export class PuppeteerBrowserProvider implements IBrowserProvider {
   async launch(): Promise<Browser> {
     return puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
   }
